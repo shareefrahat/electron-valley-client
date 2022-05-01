3adad672b19f24e207932eb0ab72b60c9da8e556
ShareefRahat <imr4h47@gmail.com>
2022-05-02 00:24:55 +0600

    responsive navbar and menu added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
+import Navbar from "../Navbar/Navbar";
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <header className="bg-white flex flex-row justify-between px-10 py-5 shadow-md">
-        <section>
-          <img className="w-[250px]" src={logo} alt="" />
-        </section>
-        <section>nav</section>
+      <header>
+        <div className="bg-white flex flex-row justify-between items-center px-4 lg:px-10 py-2 lg:py-4 shadow-md">
+          <section className="block lg:hidden">
+            <span onClick={() => setOpen(!open)}>
+              {open ? "Close" : "Menu"}
+            </span>
+          </section>
+          <section>
+            <img className="w-[200px] lg:w-[250px]" src={logo} alt="" />
+          </section>
+          <section className="lg:order-none hidden lg:block">
+            <Navbar></Navbar>
+          </section>
+          <section>
+            <Link to="/login">LOGIN</Link>
+          </section>
+        </div>
+        <div>
+          <section
+            className={`block lg:hidden absolute inset-y-0 bg-white py-5 px-10 text-left shadow-md transition-all duration-1000 ${
+              open ? "left-0 top-0" : "left-[-200px]"
+            }`}
+          >
+            <div onClick={() => setOpen(!open)}>X</div>
+            <Navbar></Navbar>
+          </section>
+        </div>
       </header>
     </>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="text-md lg:text-lg text-blue-700 flex flex-col lg:flex-row justify-around gap-5">
+        <Link to="/">HOME</Link>
+        <Link to="/">BLOG</Link>
+        <Link to="/">INVENTORY</Link>
+        <Link to="/">MY ITEMS</Link>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
