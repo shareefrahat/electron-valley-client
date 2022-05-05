76aa1c96264b4af2458a23bdb076413fa6aabafd
ShareefRahat <imr4h47@gmail.com>
2022-05-05 20:54:46 +0600

    input field and function for new amount is added

--- src/components/InventoryForm/InventoryForm.js
+++ src/components/InventoryForm/InventoryForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
+import { ArrowNarrowRightIcon, PlusIcon } from "@heroicons/react/solid";
 
 const InventoryForm = ({ product }) => {
   const { _id, name, supplier, price, quantity } = product;
@@ -9,6 +9,7 @@ const InventoryForm = ({ product }) => {
     price,
     quantity,
   });
+  const [newValue, setNewValue] = useState(0);
 
   const handleName = (e) => {
     const { name, ...rest } = update;
@@ -46,6 +47,36 @@ const InventoryForm = ({ product }) => {
     }
     const newProduct = { quantity: newQuantity, ...rest };
     setUpdate(newProduct);
+    const url = `http://localhost:5000/update/${_id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(update),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Products Quantity added");
+
+        console.log("Success:", data);
+      });
+  };
+
+  const handleNewValue = (e) => {
+    const value = parseInt(e.target.value);
+    console.log(value);
+    if (value === 0 || value === "") {
+      return;
+    }
+    setNewValue(value);
+  };
+
+  const addNewQuantity = () => {
+    const { quantity, ...rest } = update;
+    const newQuantity = quantity + newValue;
+    const newProduct = { quantity: newQuantity, ...rest };
+    setUpdate(newProduct);
   };
 
   console.log(typeof update);
@@ -63,6 +94,7 @@ const InventoryForm = ({ product }) => {
       .then((response) => response.json())
       .then((data) => {
         alert("Products updated Successfully");
+
         console.log("Success:", data);
       });
   };
@@ -151,11 +183,40 @@ const InventoryForm = ({ product }) => {
               </button>
             </div>
           </div>
+          <div className="mb-6 flex flex-row justify-between items-end">
+            <div className="w-full">
+              <label
+                htmlFor="new-quantity"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                New Quantity
+              </label>
+              <input
+                onChange={handleNewValue}
+                type="number"
+                id="new-quantity"
+                name="new-quantity"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter new amount"
+                required
+              />
+            </div>
+            <div className="w-1/2 mx-5">
+              <button
+                onClick={addNewQuantity}
+                type="button"
+                className=" text-blue-700 border border-blue-700  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Add Item
+                <PlusIcon className="ml-2 w-6 inline-block"></PlusIcon>
+              </button>
+            </div>
+          </div>
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Update
+            Update All
           </button>
         </form>
       </div>
