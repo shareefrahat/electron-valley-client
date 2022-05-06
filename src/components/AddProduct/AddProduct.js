047d2b8ab3866f73158a3183b7d3ab376cb5e9d6
ShareefRahat <imr4h47@gmail.com>
2022-05-06 10:35:36 +0600

    functionality for adding product is added

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -3,6 +3,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const AddProduct = () => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const img = e.target.img.value;
+    const supplier = e.target.supplier.value;
+    const price = e.target.price.value;
+    const quantity = e.target.quantity.value;
+    const description = e.target.details.value;
+
+    const product = { name, img, supplier, price, quantity, description };
+
+    console.log(product);
+
+    fetch("http://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Products Added Successfully");
+        e.target.reset();
+        console.log("Success:", data);
+      });
+  };
   return (
     <div>
       <div className="flex flex-row">
@@ -28,8 +55,8 @@ const AddProduct = () => {
           </div>
         </section>
         <section className="w-full px-20 py-10 ">
-          <div className="shadow-md sm:rounded-lg border border-blue-700 p-5">
-            <form>
+          <div className="shadow-md sm:rounded-lg border border-blue-700 p-5 text-left">
+            <form onSubmit={handleOnSubmit}>
               <div className="mb-6">
                 <label
                   htmlFor="event-name"
@@ -57,6 +84,54 @@ const AddProduct = () => {
                   type="text"
                   id="img-link"
                   name="img"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Paste image link here"
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="supplier"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Supplier
+                </label>
+                <input
+                  type="text"
+                  id="supplier"
+                  name="supplier"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Paste image link here"
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="price"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Price
+                </label>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Paste image link here"
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  htmlFor="quantity"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Quantity
+                </label>
+                <input
+                  type="number"
+                  id="quantity"
+                  name="quantity"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Paste image link here"
                   required
