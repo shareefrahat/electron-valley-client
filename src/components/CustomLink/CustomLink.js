2926ce8767e17dc0a8da31632b55fe4ac21c38ad
ShareefRahat <imr4h47@gmail.com>
2022-05-08 16:10:07 +0600

    custom link active route design update

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -9,8 +9,8 @@ const CustomLink = ({ children, to, ...props }) => {
       <Link
         className={
           match
-            ? "bg-slate-100 lg:bg-orange-600 px-2 py-1 rounded text-blue-700 lg:text-slate-100"
-            : "px-2 py-1 lg:hover:text-orange-600"
+            ? "bg-slate-100 lg:bg-white lg:border-2 lg:border-orange-600 px-2 py-1 rounded text-blue-700 lg:text-orange-600"
+            : "px-2 py-1 lg:hover:text-orange-600 lg:border-2 lg:border-transparent"
         }
         to={to}
         {...props}
